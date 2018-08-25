2b1abcc8bc50dbaad033098ce4228576d9040248
dolphingarlic <dolphingarlic@gmail.com>
2018-08-25 21:08:01 +0200

    Renamed snake.html to index.html and changed the slinky function to run based on time

--- snake.js
+++ snake.js
@@ -1,40 +1,54 @@
 let canvas;
 let ctx;
 
-let vectors = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],];
+let vectors = new Array();
+let length = 15;
 
-function draw(event) {
+let currentX = -1;
+let currentY = -1;
+
+function getCoords(event) {
+    currentX = event.clientX;
+    currentY = event.clientY;
+}
+
+function draw() {
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'black';
     ctx.fill();
 
-    vectors.push([event.clientX, event.clientY]);
+    vectors.push([currentX, currentY]);
     vectors.shift();
     
-    for (i = 0; i < vectors.length; i++) {
-        ctx.beginPath();
-        ctx.arc(vectors[i][0], vectors[i][1], 10 + 3 * i, 0, 2 * Math.PI, false);
+    for (i = 0; i < length; i++) {
+        if (vectors[i][0] > -1 && vectors[i][1] > -1) {
+            ctx.beginPath();
+            ctx.arc(vectors[i][0], vectors[i][1], 75 - (30 + 2 * i), 0, 2 * Math.PI, false);
 
-        ctx.fillStyle = 'red';
-        ctx.fill();
-        ctx.stroke();
+            ctx.fillStyle = 'rgb(255, ' + (20 * i) + ', 0)';
+            ctx.fill();
+        }
     }
-};
+}
 
 function init() {
     canvas = document.getElementById('snake_canvas');
     canvas.height = document.body.clientHeight;
     canvas.width = document.body.clientWidth;
 
+    for (i = 0; i < length; i++) {
+        vectors.push([currentX, currentY]);
+    }
+
     ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'green';
-    ctx.strokeStyle = '#0033';
-    ctx.lineWidth = 5;
 
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'black';
     ctx.fill();
-};
+
+    clearInterval();
+    setInterval(draw, 10);
+}
 
 onload = init;
 onresize = init;
